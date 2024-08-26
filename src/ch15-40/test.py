88d0c41c8f1b29dd851e2b977f42ec5edefd4fac
dhhan <dhhan@.covision.co.kr>
2024-08-26 17:50:30 +0900

    day 14

---
+++ src/ch15-40/test.py
@@ -0,0 +1,9 @@
+import logging
+# 색상을 적용한 로그 출력 형식 설정
+color_log_format = "\033[1;32m%(levelname)s\033[36m: %(message)s"
+logging.basicConfig(level=logging.DEBUG,format=color_log_format)
+logging.debug("이것은 디버그 메세지 입니다.")
+logging.info("이것은 정보 메시지입니다.")
+logging.warning("이것은 경고 메시지입니다.")
+logging.error("이것은 에러 메시지입니다.")
+logging.critical("이것은 심각한 에러 메시지입니다.")
